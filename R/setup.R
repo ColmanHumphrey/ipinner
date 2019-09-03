1bc41dc2805a790f3e787a83ff51d12bb45c0e89
Colman Humphrey <colmanhumphrey@gmail.com>
2019-09-03 01:48:31 -0400

    Dev (#1)
    * adding travis and codecov
    
    * Finished up the first part of the setup code

--- R/setup.R
+++ R/setup.R
@@ -5,7 +5,7 @@
 #' or exchange, and it has the dates separated. This function is to rearrange this
 #' process by combining dates (unsplitting raw data) but splitting on ad_id and
 #' exchange.
-#' 
+#'
 #' @param data_type one of bid, clk, conv, imp
 #' @param data_date date in the form YYYYMMDD
 #' @param col_names names of the columns; only bid should be different (smaller)
@@ -30,7 +30,7 @@ read_split_save <- function(data_type,
         stop("data_date ", data_date, "not recognised as format YYYYMMDD",
              call. = FALSE)
     }
-    
+
     dest_file = paste0(input_folder, data_type, '.', data_date, '.txt')
 
     if (!file.exists(dest_file)) {
@@ -69,7 +69,7 @@ read_split_save <- function(data_type,
 
                 write_file <- paste0(ad_id_dir, "/", data_type, "_exchange",
                                      ex, ".csv")
-                
+
                 data.table::fwrite(x = exchange_split[[ex]],
                                    file = write_file,
                                    append = file.exists(write_file))
@@ -80,4 +80,162 @@ read_split_save <- function(data_type,
         message("process failed with message ", e$message)
         return(FALSE)
     })
+}
+
+
+#' Takes the result of the processed files from above
+#' and combines them into one dataframe, per ad_id/exchange
+#'
+#' @param ad_id the ad_id to operate on
+#' @param exchange the exchange to use
+#' @param input_folder where the data is stored (output_folder from read_split_save)
+#' @return a data frame, the result of joining the four and calling clean_joined_data
+#' @author Colman Humphrey
+#'
+#' @export
+create_combined_data <- function(ad_id,
+                                 exchange,
+                                 input_folder){
+    if (!(ad_id %in% c("1458", "3358", "3386", "3427", "3476"))) {
+        warning("not sure if ad_id ", ad_id, " known", call. = FALSE)
+    }
+
+    if (!(exchange %in% c("1", "2", "3"))) {
+        warning("not sure if exchange ", exchange, " known", call. = FALSE)
+    }
+
+    message("combining data for ad_id ", ad_id, ", exchange ", exchange)
+
+    ##----------------
+
+    input_ad_folder <- paste0(input_folder, "/", ad_id, "/")
+
+    bid_data <- data.table::fread(paste0(input_ad_folder,
+                                         "bid_exchange", exchange, ".csv"),
+                                  integer64 = "character")
+
+    ## adding the others
+    use_cols <- c("ipinyou_id", "log_type", "paying_price", "landing_page_url")
+    for (data_type in c("clk", "conv", "imp")) {
+        input_file <- paste0(input_ad_folder, data_type,
+                             "_exchange", exchange, ".csv")
+        if (!file.exists(input_file)) {
+            message("    ", data_type, ": file not found (`",
+                    input_file, "`)")
+            for (use_col in use_cols) {
+                bid_data[[paste0(data_type, "_", use_col)]] <- NA
+            }
+        } else {
+            message("    ", data_type, ": joining")
+            dt_frame <- data.table::fread(input_file,
+                                          integer64 = "character",
+                                          select = c("bid_id", use_cols, "timestamp"))
+            names(dt_frame) <- c("bid_id", paste0(data_type, "_", use_cols), "timestamp")
+
+            ## solve duplicates by assuming it's the min timestamp
+            dt_frame <- dt_frame[order(timestamp)][!duplicated(bid_id)][, timestamp:=NULL]
+
+            bid_data <- merge(bid_data, dt_frame,
+                              by = c("bid_id"), all.x = TRUE)
+        }
+    }
+
+    message("cleaning joined data")
+    ## ok data.table sucks
+    clean_joined_data(dplyr::as_tibble(bid_data))
+}
+
+
+#' Takes first non-missing value in a series of inputs
+#'
+#' @param ... something like a bunch of vectors
+#' @return an element same length as the original, but with the first non-missing
+#' for each entry, if any (else just missing)
+#' @author Colman Humphrey
+coalesce <- function(...){
+    rel_list <- list(...)
+    if (length(unique(lengths(rel_list))) > 1L) {
+        stop("all inputs must have the same length to coalesce",
+             call. = FALSE)
+    }
+
+    pair_coalesce <- function(x, y){
+        x[is.na(x)] <- y[is.na(x)]
+        x
+    }
+
+    Reduce(pair_coalesce, rel_list)
+}
+
+#' Given a set of values of some kind, creates sawtooth summation of them
+#'
+#' @param input_vec
+#' @return vector same length as input_vec, but integers
+#' @author Colman Humphrey
+sawtooth <- function(input_vec){
+    lgl_vec <- c(FALSE,
+                 input_vec[2:length(input_vec)] ==
+                 input_vec[1:(length(input_vec) - 1)])
+    lgl_vec[is.na(input_vec)] <- FALSE
+
+    falses <- c(which(!lgl_vec), length(lgl_vec) + 1L)
+    unlist(lapply(falses[2:length(falses)] - falses[1:(length(falses) - 1)],
+                  function(len){
+                      1:len
+                  }))
+}
+
+
+#' Does further parsing of the joined data
+#'
+#' @param full_data joined data produced in create_combined_data
+#' @return dataframe with same rows as full_data, but with some new columns
+#' (and without some old ones)
+#' @author Colman Humphrey
+clean_joined_data <- function(full_data){
+    message("removing literal \"null\" strings")
+    for (col_name in names(full_data)) {
+        null_index <- full_data[[col_name]] == "null"
+        full_data[[col_name]][null_index] <- NA
+    }
+
+    full_data[["paying_price"]] <- full_data[["imp_paying_price"]]
+    full_data$clk_paying_price <- NULL
+    full_data$imp_paying_price <- NULL
+    full_data$conv_paying_price <- NULL
+
+    ## we'll keep log types, they're not the same
+    full_data[["ipinyou_id"]] <- coalesce(full_data[["ipinyou_id"]],
+                                          full_data[["imp_ipinyou_id"]],
+                                          full_data[["clk_ipinyou_id"]],
+                                          full_data[["conv_ipinyou_id"]])
+    full_data[["landing_page_url"]] <- coalesce(
+        full_data[["imp_landing_page_url"]],
+        full_data[["clk_landing_page_url"]],
+        full_data[["conv_landing_page_url"]])
+    full_data$clk_landing_page_url <- NULL
+    full_data$imp_landing_page_url <- NULL
+    full_data$conv_landing_page_url <- NULL
+
+    ##----------------
+
+    message("parsing user agent (this is slow...)")
+    ua_strings <- full_data$user_agent
+    ua_uni <- unique(ua_strings)
+
+    ua_parsed_frame = as.data.frame(uaparserjs::ua_parse(ua_uni, .progress = TRUE))
+    ua_full_frame = ua_parsed_frame[
+        match(ua_strings, ua_uni),
+        c("ua.family", "os.family", "device.family")]
+    message("user agent parsed")
+
+    full_data <- cbind(full_data, ua_full_frame)
+
+    ## tried frank to solve this, way too slow
+
+    full_data <- full_data[order(full_data$ipinyou_id,
+                                 full_data$timestamp), ]
+    full_data$user_bid_num <- sawtooth(full_data$ipinyou_id)
+
+    full_data
 }
---
+++ inst/scripts/test_setup.R
@@ -0,0 +1,107 @@
+##------------------------------------
+## Here, we'll focus on the "2nd", which we discovered was the cleanest dataset
+
+## The files are pretty big so we (probably) can't just read them all in
+
+## First part:
+## 1. read in the data, by data type and date
+## 2. split these by ad_id, exchange, (continue to split on data type), but
+##    COMBINE dates by appending
+## 3. will create folders of the form "training2nd_adsplit/<ad_id>"
+## 4. files in these folders will be "<data_type>_exchange{1, 2, 3}.csv"
+
+## Second part:
+## 1. for each ad_id, exchange, read in bid
+## 2. join the other three
+## 3. clean up and save the result
+
+##------------------------------------
+
+## Make sure:
+## devtools::install_github("ColmanHumphrey/ipinner")
+
+##------------------------------------
+
+data_folder <- "../../data/"
+
+input_folder <- paste0(data_folder, "ipinyou.contest.dataset/training2nd/")
+data_dates <- paste0("201306",
+                     formatC(6:12, width = 2, flag = "0"))
+data_types <- c("bid", "clk", "conv", "imp")
+
+## change this for a different folder if you want:
+adsplit_folder <- paste0(data_folder, "training2nd_adsplit")
+if (dir.exists(adsplit_folder)) {
+    dir.create(adsplit_folder)
+}
+
+##------------------------------------
+
+all_colnames = c("bid_id",
+                 "timestamp",
+                 "log_type", #
+                 "ipinyou_id",
+                 "user_agent",
+                 "ip",
+                 "region_id",
+                 "city_id",
+                 "ad_exchange",
+                 "domain",
+                 "url",
+                 "anon_url",
+                 "ad_slot_id",
+                 "ad_slot_width",
+                 "ad_slot_height",
+                 "ad_slot_visibility",
+                 "ad_slot_format",
+                 "ad_slot_floor_price",
+                 "creative_id",
+                 "bidding_price",
+                 "paying_price", #
+                 "landing_page_url", #
+                 "advertiser_id",
+                 "user_profile_id")
+
+not_bid <- c("log_type", "paying_price", "landing_page_url")
+bid_colnames <- all_colnames[!(all_colnames %in% not_bid)]
+
+##------------------------------------
+
+for (data_type in data_types){
+    if (data_type == "bid") {
+        col_names <- bid_colnames
+    } else {
+        col_names <- all_colnames
+    }
+
+    ## NOTE: this function actually saves the data for youx
+    lapply(data_dates, function(data_date) {
+        ipinner::read_split_save(data_type = data_type,
+                                 data_date = data_date,
+                                 col_names = col_names,
+                                 input_folder,
+                                 adsplit_folder)
+    })
+}
+
+## we just used these two for whatever reason
+ad_ids <- c("1458", "3386")
+exchanges <- c("1", "2", "3")
+
+## we'll create the combined data
+for (ad_id in ad_ids) {
+    for (exchange in exchanges) {
+        combined_data <- ipinner::create_combined_data(
+                                      ad_id = ad_id,
+                                      exchange = exchange,
+                                      input_folder = adsplit_folder)
+        data.table::fwrite(combined_data,
+                           paste0(adsplit_folder, "/", ad_id, "/",
+                                  "combined_exchange", exchange, ".csv"))        
+    }
+}
+
+## optionally you can now delete the huge extra files we've created
+## during this process
+
+##------------------------------------
